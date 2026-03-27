18ad7b465a7b5fe4503f0f98b66e2ca4732b75ec
Queiroz JR <131998280+josielqrozjr@users.noreply.github.com>
2026-03-27 23:56:51 +0000

    feat: Refactor exibirResultados function to remove assembly file generation and simplify output

--- exibirResultados.c
+++ exibirResultados.c
@@ -12,36 +12,10 @@ Grupo: RA1 9
 
 static const char *nomeArquivoAssembly = "cpulatorAssembly.s";
 
-void exibirResultados(double *resultados, int quantResultados, const char *codigoAssembly)
+void exibirResultados(double *resultados, int quantResultados)
 {
 
     printf("\n*** FASE DE COMPILACAO CONCLUIDA ***\n");
     printf("Status: SUCESSO.\n");
-
-    FILE *arquivoAssembly = fopen(nomeArquivoAssembly, "w");
-    if (arquivoAssembly != NULL)
-    {
-
-        // Escrever o header obrigatório do Assembly ARMv7
-        fprintf(arquivoAssembly, ".global _start\n");
-        fprintf(arquivoAssembly, ".text\n");
-        fprintf(arquivoAssembly, "_start:\n");
-
-        // Escrever o código que nós geramos
-        fprintf(arquivoAssembly, "%s", codigoAssembly);
-
-        // Loop infinito para segurar a placa do CPULATOR ligada no final
-        fprintf(arquivoAssembly, "fim:\n    B fim\n\n");
-
-        // TO DO: A seção .data com as variáveis será declarada aqui depois.
-
-        fclose(arquivoAssembly);
-        printf("-> Arquivo '%s' gerado com sucesso!\n", nomeArquivoAssembly);
-        printf("-> Para ver os resultados das operacoes, abra este arquivo no CPULATOR (ARMv7).\n");
-    }
-    else
-    {
-        printf("ERRO: Nao foi possivel criar o arquivo de Assembly.\n");
-    }
     printf("************************************\n");
 }
